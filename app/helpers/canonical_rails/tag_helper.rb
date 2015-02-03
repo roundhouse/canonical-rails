4b260a6b6c52e8bd57e3fe3790a88167c07d2269
Taylor j. Eke <tayloreke@gmail.com>
2015-02-03 10:52:35 -0800

    add ability to override canonical path for canonical versioning

--- app/helpers/canonical_rails/tag_helper.rb
+++ app/helpers/canonical_rails/tag_helper.rb
@@ -26,12 +26,13 @@ module CanonicalRails
       protocol
     end
 
-    def canonical_href(host=canonical_host, protocol=canonical_protocol)
-      raw "#{protocol}#{host}#{path_without_html_extension}#{trailing_slash_if_needed}#{whitelisted_query_string}"
+    def canonical_href(host=canonical_host, path=nil, protocol=canonical_protocol)
+      raw "#{protocol}#{host}#{path || path_without_html_extension}#{trailing_slash_if_needed}#{whitelisted_query_string}"
     end
 
-    def canonical_tag(host=canonical_host)
-      tag(:link, :href => canonical_href(host), :rel => 'canonical')
+    def canonical_tag(host=nil, path=nil)
+      host ||= canonical_host
+      tag(:link, :href => canonical_href(host, path), :rel => 'canonical')
     end
 
     def whitelisted_params
